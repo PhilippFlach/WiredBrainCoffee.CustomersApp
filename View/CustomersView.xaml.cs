206569ab6b96f1d9a85654439f8689f0cf22bf7d
Philipp Flach <philipp.flach@luzi.com>
2023-03-09 23:17:38 +0100

    Use the ViewModel for the CustomersView

--- View/CustomersView.xaml.cs
+++ View/CustomersView.xaml.cs
@@ -1,26 +1,38 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
-  public partial class CustomersView : UserControl
-  {
-    public CustomersView()
+    public partial class CustomersView : UserControl
     {
-      InitializeComponent();
+        private CustomersViewModel _viewModel;
+
+        public CustomersView()
+        {
+            InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
+        }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
     }
-
-    private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
-    {
-      //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
-
-      //var newColumn = column == 0 ? 2 : 0;
-      //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-      var column = Grid.GetColumn(customerListGrid);
-
-      var newColumn = column == 0 ? 2 : 0;
-      Grid.SetColumn(customerListGrid, newColumn);
-    }
-  }
 }
