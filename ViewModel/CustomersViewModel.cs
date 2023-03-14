46a9fd004dd7a95cfc816f88b06a3e99541408c2
Philipp Flach <philipp.flach@luzi.com>
2023-03-14 23:07:04 +0100

    Implementation of converter

--- ViewModel/CustomersViewModel.cs
+++ ViewModel/CustomersViewModel.cs
@@ -12,7 +12,7 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
     {
         private readonly ICustomerDataProvider _customerDataProvider;
         private CustomerItemViewModel? _selectedCustomer;
-        private int _navigationColumn;
+        private NavigationSide _navigationSide;
 
         public CustomersViewModel(ICustomerDataProvider customerDataProvider)
         {
@@ -30,12 +30,12 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             }
         }
 
-        public int NavigationColumn
+        public NavigationSide NavigationSide
         {
-            get => _navigationColumn;
+            get => _navigationSide;
             private set
             {
-                _navigationColumn = value;
+                _navigationSide = value;
                 RaisePropertyChanged();
             }
         }
@@ -67,7 +67,16 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
 
         internal void MoveNavigation()
         {
-            NavigationColumn = NavigationColumn == 0 ? 2 : 0;
+            NavigationSide = NavigationSide == NavigationSide.Left 
+                ? NavigationSide.Right 
+                : NavigationSide.Left;
         }
     }
+
+    public enum NavigationSide
+    {
+        Left, 
+        Right
+    }
+    
 }
