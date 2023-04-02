01c7564322123999644359aab08685a558313ced
Philipp Flach <philipp.flach@luzi.com>
2023-04-02 21:56:04 +0200

    Implement MainViewModel  and make LoadAsync virtual in ViewModelBase

--- ViewModel/CustomersViewModel.cs
+++ ViewModel/CustomersViewModel.cs
@@ -52,7 +52,7 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
         public DelegateCommand DeleteCommand { get; }
 
 
-        public async Task LoadAsync()
+        public async override Task LoadAsync()
         {
             if (Customers.Any())
             {
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly CustomersViewModel _customersViewModel;
+        private ViewModelBase? _selectedViewModel;
+
+        public MainViewModel(CustomersViewModel customersViewModel)
+        {
+            this._customersViewModel = customersViewModel;
+        }
+
+        public ViewModelBase? SelectedViewModel
+        {
+			get { return _selectedViewModel; }
+			set
+            {
+                _selectedViewModel = value;
+                RaisePropertyChanged();
+            }
+        }
+        public async override Task LoadAsync()
+        {
+            if (_selectedViewModel is not null) 
+            {
+                await SelectedViewModel.LoadAsync();
+            }
+        }
+	}
+}
