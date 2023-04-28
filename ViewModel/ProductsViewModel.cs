13858d9080f28425b4624c2f689d3b21b69d378b
Philipp Flach <philipp.flach@luzi.com>
2023-04-28 23:40:21 +0200

    Implemented ProductsViewModel with the LoadAsync thalt loads the products

--- ViewModel/ProductsViewModel.cs
+++ ViewModel/ProductsViewModel.cs
@@ -1,4 +1,8 @@
-﻿using WiredBrainCoffee.CustomersApp.Data;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
@@ -9,6 +13,23 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
         public ProductsViewModel(IProductDataProvider productDataProvider)
         {
             _productDataProvider = productDataProvider;
+        }
+        public ObservableCollection<Product> Products { get; } = new();
+
+        public override async Task LoadAsync()
+        {
+            if (Products.Any())
+            {
+                return;
+            }
+            var products = await _productDataProvider.GetAllAsync();
+            if (products is not null)
+            {
+                foreach (var product in products)
+                {
+                    Products.Add(product);
+                }
+            }
         }
     }
 }
